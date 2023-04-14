af575bd153b12f168dfb4552f5d811a4df5c5233
ChrisRDavila <crossdavila@gmail.com>
2023-04-14 14:22:58 -0700

    fixed test 5 to lower camel case

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -24,7 +24,7 @@ describe('AgeCalculator', () => {
   });
 
   test ("5: should convert earthAge to Jupiter Age", () => {
-    expect(ageTest.JupiterAge()).toEqual(100);
+    expect(ageTest.jupiterAge()).toEqual(100);
   });
 
 
