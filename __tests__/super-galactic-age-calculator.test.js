6a89d1fdf203937f766d87c609efa1e80542572a
ChrisRDavila <crossdavila@gmail.com>
2023-04-14 09:36:59 -0700

    had test in my js file, couldnt see end of file due to long time, moved to correct file now, test 1 set up

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -1,1 +1,12 @@
 import { AgeCalculator } from '..src/super-galactic-age-calculator.js';
+
+describe('AgeCalculator', () => {
+  let ageTest;
+
+  beforeEach(() => {
+    ageTest = new AgeCalculator(38)
+  });
+  test ('should allow to create personalized age Calculator with property for Earth age', () => {
+    expect(ageTest.age).toEqual(38);
+  });
+});
