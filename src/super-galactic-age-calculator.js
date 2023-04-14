0bf659a1f3b3de9eb902f99ee0e4c9b83c3ec0bc
ChrisRDavila <crossdavila@gmail.com>
2023-04-14 12:47:24 -0700

    test one now passing and js written

--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -1,6 +1,5 @@
 export class AgeCalculator {
   constructor(earthAge) {
-    this.earthAge = earthAge
+    this.earthAge = earthAge;
   }
-}
-//ran test again with exact age for test variable corrected
+}  
