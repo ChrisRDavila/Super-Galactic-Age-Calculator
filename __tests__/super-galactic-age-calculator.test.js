55ef0354f6bc5172546be561b38c7586a3236b3a
ChrisRDavila <crossdavila@gmail.com>
2023-04-14 14:01:03 -0700

    have been importing from wrong js file, wasnt syntax issue, took too long to figure out, test 2 updated

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -4,15 +4,15 @@ describe('AgeCalculator', () => {
   let ageTest;
 
   beforeAll(() => {
-    ageTest = new AgeCalculator(38.87)
+    ageTest = new AgeCalculator(38.866)
   });
   
   test ('1 :should allow to create personalized age Calculator with property for Earth age', () => {
-    expect(ageTest.earthAge).toEqual(38.87);
+    expect(ageTest.earthAge).toEqual(38.866);
   });
   
   test ("2 :should convert age(earthAge) to mercury Age", () => {
-    expect(ageTest.mercuryAge()).toEqual(161.96);
+    expect(ageTest.mercuryAge()).toEqual(161.94);
   });
 
 
